89a3a4ce30ed32674d00a986aeb9342bec8eab8d
R3ALCYB3R <rivalscompletion@gmail.com>
2026-03-26 20:30:38 -0400

    Create script.js

---
+++ Schoolify/script.js
@@ -0,0 +1,31 @@
+function openGame(url) {
+    document.getElementById('library').style.display = 'none';
+    document.getElementById('main-header').style.display = 'none';
+    document.getElementById('player-container').style.display = 'block';
+    document.getElementById('game-iframe').src = url;
+    document.title = "Google Docs - Editing...";
+}
+
+function closeGame() {
+    document.getElementById('library').style.display = 'grid';
+    document.getElementById('main-header').style.display = 'block';
+    document.getElementById('player-container').style.display = 'none';
+    document.getElementById('game-iframe').src = '';
+    document.title = "Google Docs - Schoolify";
+}
+
+function filterGames() {
+    let input = document.getElementById('searchBar').value.toLowerCase();
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].innerText.toLowerCase();
+        cards[i].style.display = title.includes(input) ? "" : "none";
+    }
+}
+
+// THE PANIC KEY (Escape)
+window.addEventListener('keydown', function(e) {
+    if (e.key === "Escape") {
+        window.location.href = "https://classroom.google.com";
+    }
+});
